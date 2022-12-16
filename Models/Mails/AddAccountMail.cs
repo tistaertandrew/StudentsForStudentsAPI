528cbacc561d54fc55667cf06af3cbee876870b9
Andrew Tistaert <a.tistaert@student.helmo.be>
2022-12-16 17:48:08 +0100

    MODIFY: correction erreur mail d'envoie pour la création d'un compte

--- Models/Mails/AddAccountMail.cs
+++ Models/Mails/AddAccountMail.cs
@@ -6,5 +6,5 @@ public class AddAccountMail : DefaultMail
         base(subject, to, from, values) { }
 
     public override string GetMailBody()
-        => GetMailStyle($"Bonjour {Values[0]}", "\n\nNous vous souhaitons la bienvenue sur notre plateforme StudentsForStudents !");
+        => GetMailStyle($"Bonjour {Values[0]}", "\n\nNous vous souhaitons la bienvenue sur notre plateforme Students for Students !");
 }
